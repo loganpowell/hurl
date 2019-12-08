eb974e47e22a492018b868ae92271e9af0125097
Logan Powell <loganp@tepper.cmu.edu>
2019-12-08 11:53:37 -0500

    build

--- test/meta/index.js
+++ test/meta/index.js
@@ -1,3 +1,9 @@
-import { inject_meta } from "../../src"
+export const inject_meta = (prop, content) => {
+  let meta = document
+    .createRange()
+    .createContextualFragment(`<meta property="${prop}" content="${content}"/>`)
+  document.getElementsByTagName("head")[0].appendChild(meta)
+}
+
 inject_meta("og:title", "just a test content injection")
 inject_meta("og:image", "https://i.imgur.com/BOdIBQz.gif")
